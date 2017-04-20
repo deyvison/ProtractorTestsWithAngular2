02ad5cb836bd63e7ea8a7fbcb67100222aa82a80
Josimar Alves <josimar.alves@dce.ufpb.br>
2017-04-20 15:21:38 -0300

    Codigo comentado

--- tests/spec.js
+++ tests/spec.js
@@ -22,122 +22,122 @@ describe('SFIS Manager login page', function() {
     });
 });
 
-describe('SFIS Manager login page', function() {
-    it('should to do login in the web page', function() {
-        browser.get('http://localhost:54270/main/login');
-
-        element(by.id('md-input-1')).sendKeys('annemoreira');
-        var password = element(by.id('md-input-4')).sendKeys('compal123');
-        password.sendKeys(protractor.Key.ENTER);
-
-        browser.driver.sleep(20000);
-
-        var name = browser.findElement(by.className('name'));
-        var home = browser.findElement(by.className('nav-menu-selection mat-ripple'));
-        var signOut = browser.findElement(by.className('sign-out'));
-
-        // verifica se está na página de logado
-        expect(name.getText()).toBe('Anne Lorayne Moreira');
-        expect(home.getText()).toBe('HOME');
-        expect(signOut.getText()).toBe('Sign Out');
-    });
-});
-
-
-describe('SAR main page', function() {
-    it('should to check elements count on menu', function() {
-        var home = browser.findElement(by.className('nav-menu-selection mat-ripple')).click();
-
-        var list = element(by.id('menuItems')).all(By.tagName('a'));
-        expect(list.count()).toBe(3);
-
-        //element(by.id('menuItems')).all(By.tagName('a')).get(0).click(); // click on first element on ul list
-
-        //browser.driver.sleep(20000);
-    });
-});
-
-describe('SAR main page', function() {
-    it('should to verify all items on dropdown HOME menu', function() {
-
-        var mo = element(by.id('menuItems')).all(By.tagName('a')).get(0);
-        var yes = element(by.id('menuItems')).all(By.tagName('a')).get(1);
-        var outer = element(by.id('menuItems')).all(By.tagName('a')).get(2);
-
-        expect(mo.getText()).toBe("MO");
-        expect(yes.getText()).toBe("YES");
-        expect(outer.getText()).toBe("REPAIR FROM OUTER");
-        browser.driver.sleep(20000);
-    });
-});
-
-//TODO se clicar no MO, aparecer alguma coisa, quando clicar em filters, fazer algo, quando clicar em columns, fazer algo
-describe('SAR MO page', function() {
-    it('should to show MO page when click in MO option at main page', function() {
-        browser.driver.sleep(10000); // delay to loading table
-        browser.get('http://localhost:54270/main/');
-        browser.findElement(by.className('nav-menu-selection mat-ripple')).click();
-        element(by.id('menuItems')).all(By.tagName('a')).get(0).click();
-
-        var filtersButton = browser.findElement(by.className('mat-raised-button')); // mudar o id pq tá pegando igual
-        var toolbox = element(by.id('toolbox'));
-
-        expect(filtersButton.getText()).toEqual("filter_listFilters");
-        expect(toolbox).not.toBe(null);
-
-
-    });
-});
-
-//TODO se clicar no MO, aparecer alguma coisa, quando clicar em filters, fazer algo, quando clicar em columns, fazer algo
-describe('SAR MO page', function() {
-    it('Filter result by especific MO', function() {
-        browser.driver.sleep(10000); // delay to loading table
-        browser.get('http://localhost:54270/main/');
-        browser.findElement(by.className('nav-menu-selection mat-ripple')).click();
-        element(by.id('menuItems')).all(By.tagName('a')).get(0).click();
-
-        element(by.id('filtersButton')).click();
-
-        var moInput = element(by.id("md-input-3")).sendKeys("testeeeeee"); // modificar aqui
-
-
-        browser.driver.sleep(10000);
-        // erro aqui
-
-    });
-});
-
-describe('SAR main page', function() {
-    it('Should log out on page', function() {
-
-        // fazer log out
-        browser.findElement(by.className('sign-out')).click();
-
-        var loginTextField = browser.findElement(by.className('mat-card-title'));
-        var loginButton = browser.findElement(by.className('mat-button-wrapper'));
-
-        expect(loginTextField.getText()).toEqual("Login");
-        expect(loginButton.getText()).toEqual("Login");
-    });
-
-    it ('filtrar por modelo', function() {
-      element(by.id('filters')).click();
-      var model = element(by.id('md-input-11'));
-      model.sendKeys("XXXXX");
-      model.sendKeys(protractor.Key.ENTER);
-      element(by.id('columns')).click();
-      browser.driver.sleep(20000);
-
-
-      var teste = element(by.id('listColumn')).all(By.tagName('li'));
-      var mo = teste.get(0);
-      var yes = teste.get(1);
-      var outer = teste.get(2);
-      expect(mo.getText()).toBe("MO");
-      expect(yes.getText()).toBe("YES");
-      expect(outer.getText()).toBe("REPAIR FROM OUTER");
-    });
+// describe('SFIS Manager login page', function() {
+//     it('should to do login in the web page', function() {
+//         browser.get('http://localhost:54270/main/login');
+//
+//         element(by.id('md-input-1')).sendKeys('annemoreira');
+//         var password = element(by.id('md-input-4')).sendKeys('compal123');
+//         password.sendKeys(protractor.Key.ENTER);
+//
+//         browser.driver.sleep(20000);
+//
+//         var name = browser.findElement(by.className('name'));
+//         var home = browser.findElement(by.className('nav-menu-selection mat-ripple'));
+//         var signOut = browser.findElement(by.className('sign-out'));
+//
+//         // verifica se está na página de logado
+//         expect(name.getText()).toBe('Anne Lorayne Moreira');
+//         expect(home.getText()).toBe('HOME');
+//         expect(signOut.getText()).toBe('Sign Out');
+//     });
+// });
+//
+//
+// describe('SAR main page', function() {
+//     it('should to check elements count on menu', function() {
+//         var home = browser.findElement(by.className('nav-menu-selection mat-ripple')).click();
+//
+//         var list = element(by.id('menuItems')).all(By.tagName('a'));
+//         expect(list.count()).toBe(3);
+//
+//         //element(by.id('menuItems')).all(By.tagName('a')).get(0).click(); // click on first element on ul list
+//
+//         //browser.driver.sleep(20000);
+//     });
+// });
+//
+// describe('SAR main page', function() {
+//     it('should to verify all items on dropdown HOME menu', function() {
+//
+//         var mo = element(by.id('menuItems')).all(By.tagName('a')).get(0);
+//         var yes = element(by.id('menuItems')).all(By.tagName('a')).get(1);
+//         var outer = element(by.id('menuItems')).all(By.tagName('a')).get(2);
+//
+//         expect(mo.getText()).toBe("MO");
+//         expect(yes.getText()).toBe("YES");
+//         expect(outer.getText()).toBe("REPAIR FROM OUTER");
+//         browser.driver.sleep(20000);
+//     });
+// });
+//
+// //TODO se clicar no MO, aparecer alguma coisa, quando clicar em filters, fazer algo, quando clicar em columns, fazer algo
+// describe('SAR MO page', function() {
+//     it('should to show MO page when click in MO option at main page', function() {
+//         browser.driver.sleep(10000); // delay to loading table
+//         browser.get('http://localhost:54270/main/');
+//         browser.findElement(by.className('nav-menu-selection mat-ripple')).click();
+//         element(by.id('menuItems')).all(By.tagName('a')).get(0).click();
+//
+//         var filtersButton = browser.findElement(by.className('mat-raised-button')); // mudar o id pq tá pegando igual
+//         var toolbox = element(by.id('toolbox'));
+//
+//         expect(filtersButton.getText()).toEqual("filter_listFilters");
+//         expect(toolbox).not.toBe(null);
+//
+//
+//     });
+// });
+//
+// //TODO se clicar no MO, aparecer alguma coisa, quando clicar em filters, fazer algo, quando clicar em columns, fazer algo
+// describe('SAR MO page', function() {
+//     it('Filter result by especific MO', function() {
+//         browser.driver.sleep(10000); // delay to loading table
+//         browser.get('http://localhost:54270/main/');
+//         browser.findElement(by.className('nav-menu-selection mat-ripple')).click();
+//         element(by.id('menuItems')).all(By.tagName('a')).get(0).click();
+//
+//         element(by.id('filtersButton')).click();
+//
+//         var moInput = element(by.id("md-input-3")).sendKeys("testeeeeee"); // modificar aqui
+//
+//
+//         browser.driver.sleep(10000);
+//         // erro aqui
+//
+//     });
+// });
+//
+// describe('SAR main page', function() {
+//     it('Should log out on page', function() {
+//
+//         // fazer log out
+//         browser.findElement(by.className('sign-out')).click();
+//
+//         var loginTextField = browser.findElement(by.className('mat-card-title'));
+//         var loginButton = browser.findElement(by.className('mat-button-wrapper'));
+//
+//         expect(loginTextField.getText()).toEqual("Login");
+//         expect(loginButton.getText()).toEqual("Login");
+//     });
+//
+//     it ('filtrar por modelo', function() {
+//       element(by.id('filters')).click();
+//       var model = element(by.id('md-input-11'));
+//       model.sendKeys("XXXXX");
+//       model.sendKeys(protractor.Key.ENTER);
+//       element(by.id('columns')).click();
+//       browser.driver.sleep(20000);
+//
+//
+//       var teste = element(by.id('listColumn')).all(By.tagName('li'));
+//       var mo = teste.get(0);
+//       var yes = teste.get(1);
+//       var outer = teste.get(2);
+//       expect(mo.getText()).toBe("MO");
+//       expect(yes.getText()).toBe("YES");
+//       expect(outer.getText()).toBe("REPAIR FROM OUTER");
+//     });
 
 
 
